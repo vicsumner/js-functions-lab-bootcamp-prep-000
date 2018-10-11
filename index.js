23ec6c1d75a4f54e2fd19171df1ea7d461ea7e02
Victoria L Sumner <vicsumner@gmail.com>
2018-10-11 19:29:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
+
+function happyHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
